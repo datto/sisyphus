fe5c9fc6da26c3242d855efe1334cdcde68fa862
John Seekins <jseekins@datto.com>
2021-11-24 08:36:58 -0700

    add test harness to filters.go
    Signed-off-by: John Seekins <jseekins@datto.com>

--- process_test.go
+++ process_test.go
@@ -1,3 +1,26 @@
+/*This file is part of sisyphus.
+ *
+ * Copyright Datto, Inc.
+ * Author: John Seekins <jseekins@datto.com>
+ *
+ * Licensed under the GNU General Public License Version 3
+ * Fedora-License-Identifier: GPLv3+
+ * SPDX-2.0-License-Identifier: GPL-3.0+
+ * SPDX-3.0-License-Identifier: GPL-3.0-or-later
+ *
+ * sisyphus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sisyphus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sisyphus.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package main
 
 import (
